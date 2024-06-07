196a68b5046c07d828b036a33f85d209a4c61994
shinghuey27 <shinghuey27@hotmail.com>
2024-06-07 13:54:00 +0800

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Shing Huey on 07/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
